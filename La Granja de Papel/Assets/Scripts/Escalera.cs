18302a66e64fb339371d3bd2ff821f3a22834cb2
torcu <45042365+Exater5@users.noreply.github.com>
2019-01-27 01:38:32 +0100

    uww

---
+++ La Granja de Papel/Assets/Scripts/Escalera.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Escalera : MonoBehaviour
+{
+    public Transform trArriba;
+    Transform trPlayer;
+    GameObject player;
+   
+   bool subeEscalera = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(subeEscalera == true)
+        {
+            trPlayer.position = Vector3.MoveTowards(trPlayer.position, trArriba.position, 0.1f);
+        }
+        
+    }
+    private void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            trPlayer = col.gameObject.transform;
+            player = col.gameObject;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                
+                subeEscalera = true;
+            }
+            
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        subeEscalera = false;
+    }
+}
